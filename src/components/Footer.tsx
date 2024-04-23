f23b7c13d81b003581e2051d5da21416bfc62113
Guilherme Marchioni <guilherme.m.posse@gmail.com>
2024-04-23 10:22:25 -0300

    Footer fixed in the bottom

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ interface Props {
 export default function Footer({ path_apps, path_img }: Props) {
   return (
     <div className="container">
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 border-top">
         <div className="col-md-4 d-flex align-items-center">
           <span className="mb-3 mb-md-0 text-body-secondary">
            <img src="../imgs/sitelogo.png" alt="M" id="sitelogo" /> © 2024 Marchioni.com, Inc
