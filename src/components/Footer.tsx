a87415453fdb04f3be91ad2e0983fc05fda77ba7
Guilherme Marchioni <guilherme.m.posse@gmail.com>
2024-04-23 08:59:22 -0300

    start the final, do a Single Page website

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer({ path_apps, path_img }: Props) {
       <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
         <div className="col-md-4 d-flex align-items-center">
           <span className="mb-3 mb-md-0 text-body-secondary">
-            © 2024 Marchioni.com, Inc
+           <img src="../imgs/sitelogo.png" alt="M" id="sitelogo" /> © 2024 Marchioni.com, Inc
           </span>
         </div>
         <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
